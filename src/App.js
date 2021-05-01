4022530bedc955ded8e967ae34ef50fa4f289261
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-05-01 18:25:57 +0200

    Cleaned up unused CSS

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
       <h1>Lets build a LinkedIn CLONE</h1>
-      </header>
     </div>
   );
 }
