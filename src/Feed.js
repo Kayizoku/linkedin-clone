b4ac3bb2646a9554eae30f9fd82afe5aa2851f25
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-05-02 00:41:33 +0200

    InputOptions has been styled a bit. Will continue working on it

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
+import InputOption from './InputOption';
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
 
 function Feed() {
   return <div className="feed">
@@ -11,6 +15,11 @@ function Feed() {
             <input type="text"/>
             <button type='subtmit'>Send</button>
           </form>
+      </div>
+      <div className="feed__inputOptions">
+        <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
+        <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
+        <InputOption Icon={EventNoteIcon} title='Event' color="#7FC15E" />
       </div>
     </div>
   </div>
