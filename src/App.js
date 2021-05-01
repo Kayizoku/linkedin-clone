8dad68808f4db223be7b0bfa98e6a0ca2d8af18a
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-05-02 00:24:08 +0200

    Input is added and styled. Working on the bottom options of the feed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+
 
 function App() {
   return (
--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import './Feed.ss';
+import './Feed.css';
+import CreateIcon from "@material-ui/icons/Create";
 
 function Feed() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div className="feed">
+    <div className="feed__inputContainer">
+      <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text"/>
+            <button type='subtmit'>Send</button>
+          </form>
+      </div>
+    </div>
+  </div>
 }
 
 export default Feed
