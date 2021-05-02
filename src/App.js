1790b7382255980bfd6fe8413cf738b073daf3b4
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-05-02 07:51:39 +0200

    Started working on the Login component now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 
 function App() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-
-      <div className="app__body">
+      
+      {!user ? (
+      <Login /> 
+      ) : (
+        <div className="app__body">
         <Sidebar />
         <Feed />
          {/* Sidebar*/}
         {/* Feed */}
         {/* Widgets */}
       </div>
+      )}
     </div>
   );
 }
